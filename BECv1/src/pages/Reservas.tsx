19524e2069f5189c457f0543ed5d317adf460bed
Fabi <fabianhevia758595@gmail.com>
2023-12-02 13:02:50 -0300

    Mejora de reserva
    Se agrega la solicitud y eso

--- BECv1/src/pages/Reservas.tsx
+++ BECv1/src/pages/Reservas.tsx
@@ -3,39 +3,73 @@ import './Reservas.css';
 
 const Reservas: React.FC = () => {
   return (
-    <div className="row md-6">
-      <div className="container mb-5 mt-5">
-        <div className="align-items-center">
-          <div className="d-flex justify-content-center">
-            <div className="card shadow-2-strong rounded-4">
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          {/* Primer elemento de reserva */}
+          <div className="col-md-6 mb-5">
+            <div className="position-relative start-50 ms-xxl-5 ms-xl-5 ms-sm-5 translate-middle-x">
+            <div className="ms-xxl-5 ms-xl-5 ms-lg-5 ms-sm-4">
+            <div className="ms-xxl-5 ms-xl-5 ms-lg-5">
+            <div className="card shadow-2-strong rounded-4 ms-xxl-5">
               <div className="border border-black border-top-0 border-start-0 border-end-0 border-opacity-75 border-3 rounded-4">
                 <div className="card-body p-4 text-center">
                   <h3 className="bold-text">Reservar Libro</h3>
-                  <hr></hr>
-                    <div className="form-outline mb-3">
-                      <label className="mt-3 form-label" htmlFor="ingresarID">Ingrese el ID aquí</label>
-                      <input
-                        type="number"
-                        id="ingresarID"
-                        className="form-control form-control-lg rounded-4" />
-                    </div>
-                    <div className="mb-5">
-                    </div>
-                    <hr></hr>
-                    <button
-                      className="btn btn-primary btn-lg btn-block mt-1 rounded-3"
-                      type="submit"
-                      style={{ width: '100%', color: 'white', backgroundColor: '#57412E' }}>Solicitar</button>
+                  <hr />
+                  <div className="form-outline mb-3">
+                    <label className="mt-4 form-label" htmlFor="ingresarID">Ingrese el ID aquí</label>
+                    <input
+                      type="number"
+                      id="ingresarID"
+                      className="form-control form-control-lg rounded-4"
+                    />
                   </div>
-              </div>
+                  <hr />
+                  <button
+                    className="btn btn-primary btn-lg btn-block mt-1 rounded-3"
+                    type="submit"
+                    style={{ width: '100%', color: 'white', backgroundColor: '#57412E' }}
+                  >
+                    Reservar
+                  </button>
+                </div>
               </div>
             </div>
           </div>
+          </div>
+          </div>
+          </div>
+          
+          {/* Segundo elemento de reserva */}
+          <div className="col-md-6 mb-5">
+            <div className="ms-xxl-5 ms-xl-5 ms-lg-5 ms-md-5 ms-sm-5 position-relative start-50 translate-middle-x">
+            <div className="ms-xxl-5 ms-lg-5 ms-sm-4 card shadow-2-strong rounded-4">
+              <div className="border border-black border-top-0 border-start-0 border-end-0 border-opacity-75 border-3 rounded-4">
+                <div className="card-body p-4 text-center">
+                  <h3 className="bold-text">Solicitar Prestamo</h3>
+                  <hr />
+                  <div className="form-outline mb-3">
+                    <label className="mt-4 form-label" htmlFor="ingresarID2">Ingrese el ID aquí</label>
+                    <input
+                      type="number"
+                      id="ingresarID2"
+                      className="form-control form-control-lg rounded-4"
+                    />
+                  </div>
+                  <hr />
+                  <button
+                    className="btn btn-primary btn-lg btn-block mt-1 rounded-3"
+                    type="submit"
+                    style={{ width: '100%', color: 'white', backgroundColor: '#57412E' }}
+                  >
+                    Solicitar
+                  </button>
+                </div>
+              </div>
+            </div>
         </div>
-        <div className="mt-5"></div>
-        <div className="mt-5"></div>
-        <div className="mt-5"></div>
       </div>
+    </div>
+    </div>
   );
 };
 
